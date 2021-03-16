3d46cefda3149ae9bb1642d57e96769348e6e9c0
Jonas Henrique do Nascimento <jonas.cba@gmail.com>
2021-03-16 12:04:04 -0400

    Begining study of the Interface Comparable

---
+++ src/br/com/profNelio/aula200/application/ProgramWithInterfaceComparable.java
@@ -0,0 +1,32 @@
+package br.com.profNelio.aula200.application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ProgramWithInterfaceComparable {
+
+	public static void main(String[] args) {
+		List<String> list = new ArrayList<>();
+		String path = "C:\\temp\\in2.txt";
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			
+			String name = br.readLine();
+			while (name != null) {
+				list.add(name);
+				name = br.readLine();
+			}
+			Collections.sort(list);
+			for (String string : list) {
+				System.out.println(string);
+			}
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+	}
+	
+}
