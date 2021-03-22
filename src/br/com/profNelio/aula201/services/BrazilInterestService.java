d295d5783d7c418129de50835453979ab6afa035
Jonas Henrique do Nascimento <jonas.cba@gmail.com>
2021-03-22 12:02:02 -0400

    Implementing Comparable in the entity class, comparing the attribute name and using the sort method to sort. This is a not good practice for implementing Comparable.

--- src/br/com/profNelio/aula201/services/BrazilInterestService.java
+++ src/br/com/profNelio/aula201/services/BrazilInterestService.java
@@ -1,7 +1,5 @@
 package br.com.profNelio.aula201.services;
 
-import java.security.InvalidParameterException;
-
 public class BrazilInterestService implements InterestService {
 
 	private double interestRate;
---
+++ src/br/com/profNelio/aula218/application/Program.java
@@ -0,0 +1,26 @@
+package br.com.profNelio.aula218.application;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import br.com.profNelio.aula218.entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		List<Product> list = new ArrayList<>();
+		
+		list.add(new Product("TV", 900.00));
+		list.add(new Product("Notebook", 1200.00));
+		list.add(new Product("Tablet", 450.00));
+		
+		Collections.sort(list);
+		
+		for (Product p : list) {
+			System.out.println(p);
+		}
+	}
+
+}
