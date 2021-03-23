a674ccb101d3fff18f0d3e657d7ba6191331ebd7
Jonas Henrique do Nascimento <jonas.cba@gmail.com>
2021-03-23 06:18:11 -0400

    Non-static method reference, the difference between staticProductPredicate and nonStaticProductPredicate is that the first uses the value passed by parameter, while the second uses instantiated object values.

--- src/br/com/profNelio/aula221/entities/Program.java
+++ src/br/com/profNelio/aula221/entities/Program.java
@@ -9,6 +9,7 @@ public class Program {
 
 		List<Product> list = new ArrayList<>();
 		List<Product> list2;
+		List<Product> list3;
 		
 		list.add(new Product("Tv", 900.00));
 		list.add(new Product("Mouse", 50.00));
@@ -16,11 +17,14 @@ public class Program {
 		list.add(new Product("HD Case", 80.90));
 		
 		list2 = new ArrayList<>(list);
+		list3 = new ArrayList<>(list);
 		
 		//Using ProductPredicate class
 		list.removeIf(new ProductPredicate());
 		//or
 		list2.removeIf(Product::staticProductPredicate);
+		//or
+		list3.removeIf(Product::nonStaticProductPredicate);
 		
 		for (Product p : list) {
 			System.out.println(p);
@@ -28,6 +32,10 @@ public class Program {
 		
 		System.out.println("--------------");
 		for (Product p : list2) {
+			System.out.println(p);
+		}
+		System.out.println("--------------");
+		for (Product p : list3) {
 			System.out.println(p);
 		}
 	}
