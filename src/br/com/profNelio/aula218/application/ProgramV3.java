6050f54f9efd179ef5664f33e6f8c21da816a74c
Jonas Henrique do Nascimento <jonas.cba@gmail.com>
2021-03-23 04:42:31 -0400

    Implementing Comparator in parameter pass of sort method using lambda expression. This is a good implementation because the code is clear and simple and, therefore, we have low coupling and high cohesion.

--- src/br/com/profNelio/aula218/application/ProgramV3.java
+++ src/br/com/profNelio/aula218/application/ProgramV3.java
@@ -2,6 +2,7 @@ package br.com.profNelio.aula218.application;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
 
 import br.com.profNelio.aula218.entities.Product_WithoutComparable;
@@ -11,28 +12,44 @@ public class ProgramV3 {
 	public static void main(String[] args) {
 
 		List<Product_WithoutComparable> list = new ArrayList<>();
+		List<Product_WithoutComparable> list2;
+		List<Product_WithoutComparable> list3;
 		
 		list.add(new Product_WithoutComparable("TV", 900.00));
 		list.add(new Product_WithoutComparable("Notebook", 1200.00));
 		list.add(new Product_WithoutComparable("Tablet", 450.00));
+		
+		list2 = new LinkedList<>(list);
+		list3 = new LinkedList<>(list);
 		
 		System.out.println("Before:");
 		for (Product_WithoutComparable p : list) {
 			System.out.println(p);
 		}
 		
-		//Implementing Comparable here
-		Comparator<Product_WithoutComparable> comp = new Comparator<Product_WithoutComparable>() {
-			@Override
-			public int compare(Product_WithoutComparable p1, Product_WithoutComparable p2) {
-				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-			}
+		//Below is a comparator with a lambda expression
+		Comparator<Product_WithoutComparable> comp = (p1, p2) -> {
+			return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
 		};
+		//or
+		Comparator<Product_WithoutComparable> comp2 = (p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+		//or -> adding expression lambda as parameter of the method sort
+		list3.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
+		
 		
 		list.sort(comp);
+		list2.sort(comp2);
 		
 		System.out.println("\nAfter:");
 		for (Product_WithoutComparable p : list) {
+			System.out.println(p);
+		}
+		System.out.println("\nSecond:");
+		for (Product_WithoutComparable p : list2) {
+			System.out.println(p);
+		}
+		System.out.println("\nThird:");
+		for (Product_WithoutComparable p : list3) {
 			System.out.println(p);
 		}
 	}
