043b9893e0be4b9d9111f3119f5581aff4dfc509
Jonas Henrique do Nascimento <jonas.cba@gmail.com>
2021-03-23 06:04:53 -0400

    Method reference static in the class Product, in this case, it is not using ProductPredicate.class

--- src/br/com/profNelio/aula221/entities/Program.java
+++ src/br/com/profNelio/aula221/entities/Program.java
@@ -8,15 +8,26 @@ public class Program {
 	public static void main(String[] args) {
 
 		List<Product> list = new ArrayList<>();
+		List<Product> list2;
 		
 		list.add(new Product("Tv", 900.00));
 		list.add(new Product("Mouse", 50.00));
 		list.add(new Product("Tablet", 350.00));
 		list.add(new Product("HD Case", 80.90));
 		
+		list2 = new ArrayList<>(list);
+		
+		//Using ProductPredicate class
 		list.removeIf(new ProductPredicate());
+		//or
+		list2.removeIf(Product::staticProductPredicate);
 		
 		for (Product p : list) {
+			System.out.println(p);
+		}
+		
+		System.out.println("--------------");
+		for (Product p : list2) {
 			System.out.println(p);
 		}
 	}
