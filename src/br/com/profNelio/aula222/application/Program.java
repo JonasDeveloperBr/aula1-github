7926a2dad601f3a9572bf29d83262e76cf2e6052
Jonas Henrique do Nascimento <jonas.cba@gmail.com>
2021-03-23 09:42:39 -0400

    Second example using reference method with static method.

--- src/br/com/profNelio/aula222/application/Program.java
+++ src/br/com/profNelio/aula222/application/Program.java
@@ -13,19 +13,28 @@ public class Program {
 
 		Locale.setDefault(Locale.US);
 		List<Product> list = new ArrayList<>();
+		List<Product> list1;
 		
 		list.add(new Product("Tv", 900.00));
 		list.add(new Product("Mouse", 50.00));
 		list.add(new Product("Tablet", 350.00));
 		list.add(new Product("HD Case", 80.90));
 
+		list1 = new ArrayList<>(list);
+		
 		System.out.println("Before of the update");
 		list.forEach(System.out::println);
 
-		list.forEach(new PriceUpdate());
+		list.forEach(new PriceUpdate());	//Do the update
 		
 		System.out.println("\nAfter of the update");
 		list.forEach(System.out::println);
+		
+		System.out.println("-----------------");
+		
+		list1.forEach(Product::staticPriceUpdate);	//Do the update using reference method with static method
+		list1.forEach(System.out::println);			//Print list
+		
 	}
 
 }
