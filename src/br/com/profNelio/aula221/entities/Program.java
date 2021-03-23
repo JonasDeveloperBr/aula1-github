3c7a7fc2c118b3631d301258683204b2454970e6
Jonas Henrique do Nascimento <jonas.cba@gmail.com>
2021-03-23 06:26:06 -0400

    Declared lambda expression - Creating an anonymous function with lambda expression and implementing in the object Predicate the rule.

--- src/br/com/profNelio/aula221/entities/Program.java
+++ src/br/com/profNelio/aula221/entities/Program.java
@@ -2,14 +2,14 @@ package br.com.profNelio.aula221.entities;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Program {
 
 	public static void main(String[] args) {
 
 		List<Product> list = new ArrayList<>();
-		List<Product> list2;
-		List<Product> list3;
+		List<Product> list2, list3, list4;
 		
 		list.add(new Product("Tv", 900.00));
 		list.add(new Product("Mouse", 50.00));
@@ -18,6 +18,7 @@ public class Program {
 		
 		list2 = new ArrayList<>(list);
 		list3 = new ArrayList<>(list);
+		list4 = new ArrayList<>(list);
 		
 		//Using ProductPredicate class
 		list.removeIf(new ProductPredicate());
@@ -25,6 +26,9 @@ public class Program {
 		list2.removeIf(Product::staticProductPredicate);
 		//or
 		list3.removeIf(Product::nonStaticProductPredicate);
+		//or
+		Predicate<Product> pred = p -> p.getPrice() >= 100.0;
+		list4.removeIf(pred);
 		
 		for (Product p : list) {
 			System.out.println(p);
@@ -36,6 +40,10 @@ public class Program {
 		}
 		System.out.println("--------------");
 		for (Product p : list3) {
+			System.out.println(p);
+		}
+		System.out.println("--------------");
+		for (Product p : list4) {
 			System.out.println(p);
 		}
 	}
